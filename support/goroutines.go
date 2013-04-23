47df3976c4aad31998030a7c6ded83332cc31b00
Andrew Gerrand <adg@golang.org>
2013-04-23 13:18:03 +0200

    pass duration to say func

--- support/goroutines.go
+++ support/goroutines.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	go say("let's go!", 3)
-	go say("ho!", 2)
-	go say("hey!", 1)
+	go say("let's go!", 3*time.Second)
+	go say("ho!", 2*time.Second)
+	go say("hey!", 1*time.Second)
 	time.Sleep(4 * time.Second)
 }
 
-func say(text string, secs int) {
-	time.Sleep(time.Duration(secs) * time.Second)
+func say(text string, delay time.Duration) {
+	time.Sleep(delay)
 	fmt.Println(text)
 }
